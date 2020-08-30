e05da86ec01da40626117f6a08037af4bfd86831
David Szigecsan <david.szigecsan@emarsys.com>
2020-08-30 13:14:17 +0200

    Add first test case

--- test/MutationTesting/CalculatorTest.php
+++ test/MutationTesting/CalculatorTest.php
@@ -6,4 +6,19 @@ use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
+
+    /**
+     * @var Calculator
+     */
+    private $calculator;
+
+    protected function setUp(): void
+    {
+        $this->calculator = new Calculator();
+    }
+
+    public function testAdd001(): void
+    {
+        $this->assertEquals(0, $this->calculator->add(0, 0));
+    }
 }
